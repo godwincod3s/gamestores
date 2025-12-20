f5a8b44a2a21760a23ef5b0a3d3a629b71d4f4c4
OG_hpebook <godwinikechukwu.dev@gmail.com>
2025-12-20 21:15:37 +0100

    handling cart fetch from wordpress

--- app/(routes)/account/cart/page.tsx
+++ app/(routes)/account/cart/page.tsx
@@ -39,7 +39,7 @@ export default function CartPage() {
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_WP_API_URL}/wp-json/wc/store/cart`,
         {
-          method: "POST",
+          method: "GET",
           credentials: "include",
           headers: {
             "Content-Type": "application/json",
